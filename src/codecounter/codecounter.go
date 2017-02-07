b9da58780f92a1d04935e8365fc96e4b36ff8d85
lioneagle <leoeagle@163.com>
2017-02-08 00:50:45 +0800

    add codetype stat

--- src/codecounter/codecounter.go
+++ src/codecounter/codecounter.go
@@ -55,6 +55,19 @@ func (c *CodeTypeMap) AddCodeType(filters string, codetype string) {
 	}
 }
 
+type CodeTypeStat struct {
+	filenum int
+	stat    counter.CodeStat
+}
+
+type CodeTypeStats struct {
+	maps map[string]*CodeTypeStat
+}
+
+func NewCodeTypeStats() *CodeTypeStats {
+	return &CodeTypeStats{maps: make(map[string]*CodeTypeStat)}
+}
+
 func main() {
 	filter := "*.cpp;*.cxx;*.go"
 	filters := strings.Split(filter, ";")
@@ -63,18 +76,22 @@ func main() {
 	codetypeMap.AddCodeType("*.cpp;*.cxx;*.hpp;*.hxx;*.c++;*.cc;*.c;*.h", "cpp")
 	codetypeMap.AddCodeType("*.go", "go")
 
+	codetypes := []string{"cpp", "go"}
+
 	root := "F:/dev/go_code/src/codecount"
 
 	files := &fileList{}
 
 	getFiles(root, filters, files)
 
-	total_stat := &counter.CodeStat{}
+	totlStat := &counter.CodeStat{}
+	codetypeStats := NewCodeTypeStats()
 
 	factory := counter.NewCodeCounterFactory()
 
 	for _, v := range files.data {
-		codetype, ok := codetypeMap.maps[strings.ToLower(filepath.Ext(v.filename)[1:])]
+		v.codetype = strings.ToLower(filepath.Ext(v.filename)[1:])
+		codetype, ok := codetypeMap.maps[v.codetype]
 		if !ok {
 			log.Printf("ERROR: unknown code type for %s", v.filename)
 			continue
@@ -90,10 +107,22 @@ func main() {
 		if !ok {
 			log.Printf("ERROR: parse file %s failed", v.filename)
 		}
-		total_stat.Add(&stat)
+		totlStat.Add(&stat)
+
+		codetypeStat, ok := codetypeStats.maps[v.codetype]
+		if !ok {
+			codetypeStat = &CodeTypeStat{}
+			codetypeStats.maps[v.codetype] = codetypeStat
+		}
+
+		codetypeStat.filenum++
+		codetypeStat.stat.Add(&stat)
 
 		fmt.Printf("%s: %s\n", v.filename, stat.String())
 	}
 
-	fmt.Printf("total %d files: %s\n", len(files.data), total_stat.String())
+	for _, v := range codetypes {
+		fmt.Printf("total %d %s files: %s\n", codetypeStats.maps[v].filenum, v, codetypeStats.maps[v].stat.String())
+	}
+	fmt.Printf("total %d files: %s\n", len(files.data), totlStat.String())
 }
